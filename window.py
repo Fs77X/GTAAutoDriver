e3a5245ebbda1adb0d90d5ffc231c82ef2d5dc2b
Farhan Saeed <farhansaeed1999@gmail.com>
2023-03-29 22:48:50 -0700

    added lines to road

--- window.py
+++ window.py
@@ -4,12 +4,44 @@ import cv2
 import time
 import ctypes
 import ctypes.wintypes
+from directkeys import PressKey, ReleaseKey, W, A, S, D
+
+def roi(img, vertices):
+    #blank mask
+    mask = np.zeros_like(img)
+
+    #fill the mask
+    cv2.fillPoly(mask, [vertices], 255)
+
+    # now only show area that is the mask (the road in this case)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            cords = line[0]
+            #x1, x2, y1, y2, color, thickness
+            cv2.line(img, (cords[0], cords[1]), (cords[2], cords[3]), [255, 255, 255], 3)
+    except:
+        pass
+    
 
 # retrieve edges of image via grayscale and canny edge detection
 def process_img(image):
     #gray scale image
     proccessed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     proccessed_img = cv2.Canny(proccessed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
+
+    # add gaus blur to improve line detection
+    proccessed_img = cv2.GaussianBlur(proccessed_img,(5,5),0)
+    proccessed_img = roi(proccessed_img, vertices)
+
+    # coordinates of lines detected from the canny edge detection
+    lines = cv2.HoughLinesP(proccessed_img, 1, np.pi/180, 180, 20, 15)
+    # draw lines on image
+    draw_lines(proccessed_img, lines)
     return proccessed_img
 
 # https://stackoverflow.com/questions/7142342/get-window-position-size-with-python# thanks to ShortArrow's answer!
@@ -33,10 +65,11 @@ def main():
         h, w, _ = screen.shape
         screen = screen[h - 600:, w - 800:, ]
 
-        print('loop took {} seconds'.format(time.time()-last_time))
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
         new_screen = process_img(screen)
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('window', new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
